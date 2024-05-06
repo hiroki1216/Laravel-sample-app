01f42901af93f5962b69ae26cb5b50848722263f
hiroki1216 <g.o.b96just.uw@icloud.com>
2024-05-06 23:15:58 +0900

    seederファイルを作成 .

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'John Doe',
+                'email' => 'johndoe@example.com',
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => 'Jane Doe',
+                'email' => 'janedoe@example.com',
+                'password' => Hash::make('password'),
+            ],
+        ]);
+    }
+    
+}
